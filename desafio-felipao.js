2baa0314cb53e7cf19670128d29eb399271deb03
youngalexsandro <alexsandrojose1740@gmail.com>
2023-09-29 00:20:35 -0300

    Projeto DIO

---
+++ desafio-felipao.js
@@ -0,0 +1,27 @@
+let nome = "Mario" // adicione o nome 
+let xp = 9999
+
+if (xp <= 1000){
+    console.log("O Herói " + nome + " está no nível de: " + "Ferro")
+}
+if (xp >= 1001 && xp <= 2000){
+    console.log("Bronze")
+}
+if (xp >= 2001 && xp <= 6000){
+    console.log("O Herói " + nome + " está no nível de: " + "Prata")
+}
+if (xp >= 6001 && xp <= 7000){
+    console.log("O Herói " + nome + " está no nível de: " + "Ouro")
+}
+if (xp >= 7001 && xp <= 8000){
+    console.log("O Herói " + nome + " está no nível de: " + "Platina")
+}
+if (xp >= 8001 && xp <= 9000){
+    console.log("O Herói " + nome + " está no nível de: " + "Ascendente")
+}
+if (xp >= 9001 && xp <= 10000){
+    console.log("O Herói " + nome + " está no nível de: " + "Imortal")
+}
+if (xp >= 10001){
+    console.log("O Herói " + nome + " está no nível de: " + "Radiante")
+}
